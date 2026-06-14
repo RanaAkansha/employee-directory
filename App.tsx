ac665bc552e24e4b3a4eac6e985f2d5929210526
Akansha Rana <148575796+RanaAkansha@users.noreply.github.com>
2026-06-14 18:18:47 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,20 @@
 import (useEffect, useState} from "react";
-function App( {
-}
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [searcg, setSearch] = useState(" ");
+
+
+  useEnffect(() => {
+    fetch("employees")
+          .then (res = > res.josn()).then(data => setEmployees(data));}, []);
+
+  const filtered = employees.filter(e:any) => 
+  e.name.toLowerCase().includes(toLowerCase()) || 
+    e.department.toLowerCase().includes(search.toLowerCase()));
+
+return <div>
+  <input placdeholder= 'search' onChange = {(e)} = > setSearch(e.taregt.value)} />
+  {filtered.mao((e.any) => (
+  <div key={e.id}>
+    <h3></h3>
+</div>
