03bd9fe30d72cffe9f15fd404923d64ad491da6b
Akansha Rana <148575796+RanaAkansha@users.noreply.github.com>
2026-06-14 18:27:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,34 @@ if (!employee) {
   return res.status(404).json({messgae:"Not Found"});
   res.json(employee);
 
-app.post("employees", (req,res) => 
+app.post("employees", (req,res) =>  {const {name,department} = req.body;
+if (!name|| department) {
+  return res.status(400).json ({message: "invalid data"{);
+}
+
+                                     const employee = {
+                                       id:Date.now(),
+                                       name,department};
+                                     employees.push(employee):
+                                     res.status(201).json(employee):});
+
+
+  app.put ("/employees/:id", (req,res) => {
+    const emplooyee = employees.find(e=> e.id == req.params.id );
+
+
+    if (!employee) {
+      return res.status(404).json({message: "Not Found});
+
+employee.name = req.body.naem || employee.name;
+        employee.department = req.body.department || employee.departmen;
+
+      res.json(employee);
+    });
+
+    app.delete("/employees/:id", (req,res) => {
+      employees = employees.filter( e=> e.id != req.params.id);
+
+      res.json({message: "Deleted"});
+    });
+  app.listen(3000);
